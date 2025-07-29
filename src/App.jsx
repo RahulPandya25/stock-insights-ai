f37a48f31f881d876230b0b700d67fbb0c203863
Rahul Pandya <rahulpandya25796@gmail.com>
2025-07-29 00:08:44 -0400

    update styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,37 @@ import { AlertMessage } from "./components/AlertMessage/AlertMessage";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Title } from "./common/components/Title/Title";
 import { Form } from "./common/components/Form/Form";
-import { Card } from "./common/components/Card/Card";
 import { useDispatchQueryStatus } from "./common/hooks/useDispatchQueryStatus";
 import { askAIThunk } from "./store/slices/alertSlice";
 import { useState } from "react";
 
 const App = () => {
   const [queryRunner, isRunning] = useDispatchQueryStatus(askAIThunk);
-  const [insights, setInsights] = useState({});
+  const [insights, setInsights] = useState();
   return (
     <>
       <AlertMessage />
       <div className={styles.app}>
-        <Title>Stock Insights AI</Title>
-        <Card>
-          <Form
-            fields={["Ticker"]}
-            requiredFields={["Ticker"]}
-            primaryCTA="Get Insights.."
-            onSubmit={(data) => setInsights(queryRunner(data["Ticker"]))}
-          />
-          {isRunning && "Loading..."}
-          {insights && <pre>{JSON.stringify(insights, null, 2)}}</pre>}
-        </Card>
+        <Title>📈 Stock Insights AI</Title>
+        <Form
+          className={styles.form}
+          fields={["Ticker"]}
+          requiredFields={["Ticker"]}
+          primaryCTA="Get Insights.."
+          onSubmit={(data) => {
+            queryRunner(data).then((resp) => {
+              if (resp.meta.requestStatus === "fulfilled") {
+                setInsights(resp.payload);
+              } else {
+                setInsights("Something went wrong, please try again.");
+              }
+            });
+          }}
+        />
+        <br />
+        {isRunning
+          ? "Loading..."
+          : insights && <pre>{JSON.stringify(insights, null, 2)}</pre>}
       </div>
     </>
   );
